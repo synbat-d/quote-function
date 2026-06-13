6dcc0e616fbac33ef1af0690d538c3e59e055377
Symbat Dulatuly <synbat1999@gmail.com>
2026-06-13 11:24:24 +0500

    first commit

---
+++ src/test/java/com/polarbookshop/quote_function/QuoteFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.quote_function;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteFunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
